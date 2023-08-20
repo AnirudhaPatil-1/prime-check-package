a1f41ded5f4aea7f532060f6bddea2d4c422cfb5
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-21 01:07:10 +0530

    logic improved, added tests - isPrime function improved, add tests to test file

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 //return true if the number is prime else false
 //here number is represented as 'n'
 function isPrime(n){
-    //check if numb is less than 0 or (n is even and is not 2)
-    if(n < 0 || (n % 2 === 0 && n !== 2)){
-        //n is not prime
+
+    //check if n is number and not a string and not an integer
+    //if n is not a number or not a integer or less than 2 then return false
+    if(typeof n !== 'number' || !Number.isInteger(n) || n < 2){
         return false;
-    };
+    }
+
+    //check if n is equal to 2, if yes 2 is only even prime number
+    if(n === 2){
+        return true;
+    }
+    
+    //check if n is even number, if yes then n is not a prime number
+    if(n % 2 === 0){
+        return false;
+    }
 
     //iterate from 3 to square root of n, incrementing by 2
-    for(let i = 3; i < Math.sqrt(n); i += 2){
+    for(let i = 3; i <= Math.sqrt(n); i += 2){
         //check if n is divisible by values of i
         if(n % i === 0){
             //n is not prime as it is completely divisible
